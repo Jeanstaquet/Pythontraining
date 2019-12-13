d7507cc77238014daa539ef87a70f9e0b3b85ccd
Jean Staquet <jean.staquet8@gmail.com>
2019-12-14 00:17:05 +0100

    ajout déplacement ennemi, fond, préparation pour les balles

--- Pygame_tutorial.py
+++ Pygame_tutorial.py
@@ -1,10 +1,15 @@
 import pygame
+import random
 
 # Initialize the pygame
 pygame.init()
 
 # create the screen
 screen = pygame.display.set_mode((800, 600))
+
+# Background
+background = pygame.image.load('background.png')
+
 
 # Title and icon of the window
 pygame.display.set_caption("Space Invaders")
@@ -19,9 +24,10 @@ playerX_change = 0
 
 # Enemy
 enemyImg = pygame.image.load('pyramid.png')
-enemyX = 370
-enemyY = 50
-enemyX_change = 0
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 4
+enemyY_change = 40
 
 
 def player(x, y):
@@ -37,6 +43,8 @@ running = True
 while running:
     # RGB
     screen.fill((128, 128, 0))
+    # Background image
+    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -44,19 +52,31 @@ while running:
         # if keystroke is pressed check wether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -5
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 5
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+    # Checking for boundaries of spaceship
     playerX += playerX_change
 
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
+
+    # Enemy Movement
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 4
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -4
+        enemyY += enemyY_change
+
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     pygame.display.update()
