4db1cef13e97c2341a002ddd9c928e211f36638f
Jean Staquet <jean.staquet8@gmail.com>
2019-12-13 23:42:43 +0100

    Bordures, ajout ennemi, déplacement de la navette

--- Pygame_tutorial.py
+++ Pygame_tutorial.py
@@ -1,21 +1,62 @@
 import pygame
 
-#Initialize the pygame
+# Initialize the pygame
 pygame.init()
 
-#create the screen
+# create the screen
 screen = pygame.display.set_mode((800, 600))
 
-#Title and icon of the window
+# Title and icon of the window
 pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load('balaclava.png')
+icon = pygame.image.load('pyramid.png')
 pygame.display.set_icon(icon)
 
-#Game loop
+# Player
+playerImg = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+# Enemy
+enemyImg = pygame.image.load('pyramid.png')
+enemyX = 370
+enemyY = 50
+enemyX_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+
+# Game loop
 running = True
 while running:
+    # RGB
+    screen.fill((128, 128, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    #RGB
-    screen.fill((0, 0, 0))
+
+        # if keystroke is pressed check wether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
+    pygame.display.update()
