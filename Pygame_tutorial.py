98ca46bd56bf3080d520d874028405375b9e0f15
Jean Staquet <jean.staquet8@gmail.com>
2019-12-13 15:17:12 +0100

    Mise en place des détails de la fenêtre

--- Pygame_tutorial.py
+++ Pygame_tutorial.py
@@ -1,1 +1,21 @@
 import pygame
+
+#Initialize the pygame
+pygame.init()
+
+#create the screen
+screen = pygame.display.set_mode((800, 600))
+
+#Title and icon of the window
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('balaclava.png')
+pygame.display.set_icon(icon)
+
+#Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    #RGB
+    screen.fill((0, 0, 0))
