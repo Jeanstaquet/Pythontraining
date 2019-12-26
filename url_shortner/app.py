b6d034eba147c9880788864fb43ed9f523fcdfbf
Jean Staquet <jean.staquet8@gmail.com>
2019-12-26 13:44:05 +0100

    Finition du projet, ajout layout boostrap et autres éléments graphiques

--- url_shortner/app.py
+++ url_shortner/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -8,7 +8,7 @@ app.secret_key = 'h432hi5ohi3h5i5hi3o2hi'
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', codes=session.keys())
 
 @app.route('/your-url', methods=['GET','POST'])
 def your_url():
@@ -34,6 +34,7 @@ def your_url():
 
         with open('urls.json','w') as url_file:
             json.dump(urls, url_file)
+            session[request.form['code']] = True
         return render_template('your_url.html', code=request.form['code'])
     else:
         return redirect(url_for('home'))
@@ -48,4 +49,17 @@ def redirect_to_url(code):
                 if 'url' in urls[code].keys():
                     return redirect(urls[code]['url'])
                 else:
-                    
+                    return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
+    return abort(404)
+
+#Error handler for the code 404
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+@app.route('/api')
+def session_api():
+    return jsonify(list(session.keys()))
+
+if __name__ == "__main__":
+    main()
